0016a59437079f809179d217b84b79503fa84b19
Wayne de Villiers <wrdevilliers@gmail.com>
2024-10-28 13:06:35 +0100

    footer component from Daisy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Hero from './pages/Hero.jsx';
-
+import Footer from './components/Footer.jsx';
 const App = () => {
     return (
         <>
             <Hero />
+            <Footer/>
         </>
     );
 };
