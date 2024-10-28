05864d69e5daac0cbcd31b70181a37d29404ec91
Wayne de Villiers <wrdevilliers@gmail.com>
2024-10-28 12:56:24 +0100

    navbar compponent

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+
+const Navbar = () => {
+    return (
+
+        <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">Logo</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li><a>Events</a></li>
+      <li><a>Login / Sign up</a></li>
+    </ul>
+  </div>
+</div>
+    );   
+}
+
+export default Navbar;
+
