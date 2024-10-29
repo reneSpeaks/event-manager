4498f963b66747152c5c8c258e883331e1e2755f
Wayne de Villiers <wrdevilliers@gmail.com>
2024-10-29 15:24:06 +0100

    uodate Event card

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,24 +1,28 @@
-const EventCard = ({event}) => {    
-    if (!event) {
-        return null;
-    }      
+import { useNavigate } from "react-router-dom";
+const EventCard = ({ event }) => {
+  const navigate = useNavigate();
+  if (!event) {
+    return null;
+  }
 
-    console.log(event);
+  const handleCardClick = () => {
+    navigate(`/event/${event.id}`);
+  };
 
-    return (
+  return (
     <div className="event-card bg-base-100 shadow-sm shadow-accent rounded-lg p-5 max-w-sm transform transition duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:shadow-primary">
-            <img
-                src={event.imageUrl}
-                alt={event.title}
-                className="w-full h-40 rounded-lg object-cover mb-4"
-            />
-         <div className="flex justify-center">
-                <button className="btn btn-accent">
-                    Count Me In!
-                </button>
-            </div>
-        </div>
-    );  
-}
+      <img
+        src={event.imageUrl}
+        alt={event.title}
+        className="w-full h-40 rounded-lg object-cover mb-4"
+      />
+      <div className="flex justify-center">
+        <button onClick={handleCardClick} className="btn btn-accent">
+          Count Me In!
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default EventCard;
