70918723de58bed81c3eb5edc32525527020f00f
René Weiberlenn <rene.weiberlenn@live.de>
2024-10-29 14:51:47 +0100

    preliminary typerwriter animation for hero section

---
+++ src/utils/typewriter.js
@@ -0,0 +1,13 @@
+export function typeWriter(text, i, fnCallback) {
+    if (i < text.length) {
+        document.querySelector(".typewrite").innerHTML = text.substring(0, i+1);
+
+        setTimeout(function() {
+            typeWriter(text, i + 1, fnCallback)
+        }, 100);
+    }
+
+    else if (typeof fnCallback === 'function') {
+        setTimeout(fnCallback, 700);
+    }
+}
