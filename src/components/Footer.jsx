ec54f95888150bf3cf2f25e22e62b8acf168f26b
René Weiberlenn <rene.weiberlenn@live.de>
2024-10-29 22:42:13 +0100

    adjusted height of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="footer fixed bottom-0 left-0 footer-center bg-base-300 text-base-content p-4 z-0">
+        <footer className="footer fixed h-24 bottom-0 left-0 footer-center bg-base-300 text-base-content p-4 z-0">
             <aside>
                 <p>Copyright © {new Date().getFullYear()} - All right reserved by Eventify Ltd.</p>
             </aside>
