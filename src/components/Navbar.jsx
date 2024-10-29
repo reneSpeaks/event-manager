3ffc5d264b4d979ff0cff48d698a7cad0df52b6c
René Weiberlenn <rene.weiberlenn@live.de>
2024-10-29 13:38:26 +0100

    setup for preliminary drawer for mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,33 @@
+import {FaBars} from "react-icons/fa6";
+
 const Navbar = () => {
     return (
-        <nav className="navbar flex justify-between max-w-7xl">
-            <div>
-                <a className="text-2xl font-bold cursor-pointer">Eventify<span className="text-accent">.</span></a>
+        <>
+            <div className="drawer max-w-full w-full flex justify-center">
+                <nav className="navbar flex justify-between max-w-7xl px-4">
+                    <label htmlFor="my-drawer" className="sm:hidden"><FaBars /></label>
+                    <div>
+                        <a className="text-3xl sm:text-2xl font-bold cursor-pointer hover:text-accent">Eventify<span className="text-accent">.</span></a>
+                    </div>
+                    <ul className="hidden sm:flex flex-row gap-20 font-bold">
+                        <li><a className="menu-item cursor-pointer">Home</a></li>
+                        <li><a className="menu-item cursor-pointer">Events</a></li>
+                    </ul>
+                    <button className="text-xl cursor-pointer font-bold hover:text-accent active:text-primary" onClick={() => document.getElementById('sign-form').showModal()}>Log
+                        In
+                    </button>
+                </nav>
+
+                <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                        <li><a className="menu-item cursor-pointer">Home</a></li>
+                        <li><a className="menu-item cursor-pointer">Events</a></li>
+                    </ul>
+                </div>
             </div>
-            <ul className="flex flex-row gap-20 font-bold">
-                <li><a className="menu-item cursor-pointer">Home</a></li>
-                <li><a className="menu-item cursor-pointer">Events</a></li>
-            </ul>
-            <button className="text-xl cursor-pointer font-bold hover:text-accent active:text-primary" onClick={() => document.getElementById('sign-form').showModal()}>Log In</button>
-        </nav>
+        </>
     );
 };
 
