dd4d2af242217e27fe1b4b63c89dd58ea9829d15
René Weiberlenn <rene.weiberlenn@live.de>
2024-10-29 16:09:08 +0100

    setup of api calls for events on hero page. needs to be refactored at some point

--- src/components/EventCardContainer.jsx
+++ src/components/EventCardContainer.jsx
@@ -1,16 +1,32 @@
 import EventSection from "./EventSection";
-import events from "../data/events";
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const EventCardContainer = () => {
-  return (
-    <div className="bg-gray-900 text-white py-10">
-      <h2 className="text-3xl font-semibold text-center mb-10 text-gray-300">Upcoming Events</h2>
-      
-      <EventSection event={events[0]} isReversed={false} />
-      <EventSection event={events[1]} isReversed={true} />
-      <EventSection event={events[2]} isReversed={false} />
-    </div>
-  );
+    const [upcomingEvents, setUpcomingEvents] = useState([]);
+
+    useEffect(() => {
+        axios('http://localhost:3001/api/events?page=1&limit=3')
+            .then((response) => {
+                setUpcomingEvents(response.data.results);
+                // console.log(upcomingEvents);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    return (
+        <div className="bg-gray-900 text-white py-10">
+            <h2 className="text-3xl font-semibold text-center mb-10 text-gray-300">Upcoming Events</h2>
+
+            {upcomingEvents && upcomingEvents.map((upcomingEvent, index) => {
+                return (
+                    <EventSection event={upcomingEvent} isReversed={(index % 2 === 0)} />
+                );
+            })}
+        </div>
+    );
 };
 
 export default EventCardContainer;
--- src/components/UpcomingEventCard.jsx
+++ src/components/UpcomingEventCard.jsx
@@ -1,25 +1,43 @@
-import events from "../data/events";
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 
-const UpcomingEventCard = () => {  
-  const event = events[0];
+const UpcomingEventCard = () => {
+    const [upcomingEvent, setUpcomingEvent] = useState([]);
 
-  return (
-    <div className="event-card bg-base-100 shadow-sm shadow-accent rounded-lg p-5 max-w-sm transform transition duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:shadow-primary">
-      <img
-        src={event.imageUrl}
-        alt={event.title}
-        className="w-full h-40 rounded-lg object-cover mb-4"
-      />
-      <h2 className="text-xl text-accent font-semibold mb-2">{event.title}</h2>
-      <p className="mb-1">{event.date}</p>
-      <p className="mb-1">{event.time}</p>
-      <p className="mb-1">{event.location}</p>
-      <p className="mb-4">{event.description}</p>
-      <button className="btn btn-accent">
-        Count Me In!
-      </button>
-    </div>
-  );
+    useEffect(() => {
+        // FIND UPCOMING EVENT
+        axios('http://localhost:3001/api/events/upcoming')
+            .then((response) => {
+                setUpcomingEvent(response.data[0]);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+        return () => {
+        };
+    }, []);
+
+    return (
+        <>
+            {upcomingEvent && (
+                <div className="event-card bg-base-100 shadow-sm shadow-accent rounded-lg p-5 max-w-sm transform transition duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:shadow-primary">
+                    <img
+                        src="https://via.assets.so/img.jpg?w=500&h=300"
+                        alt={upcomingEvent.title}
+                        className="w-full h-40 rounded-lg object-cover mb-4"
+                    />
+                    <h2 className="text-xl text-accent font-semibold mb-2">{upcomingEvent.title}</h2>
+                    <p className="mb-1">{upcomingEvent.date}</p>
+                    <p className="mb-1">{upcomingEvent.location}</p>
+                    <p className="mb-4">{upcomingEvent.description}</p>
+                    <button className="btn btn-accent">
+                        Count Me In!
+                    </button>
+                </div>
+            )};
+        </>
+    );
 };
 
 export default UpcomingEventCard;
