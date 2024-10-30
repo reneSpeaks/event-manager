8618d60e4a372ab41cc7020799138296ed873249
René Weiberlenn <rene.weiberlenn@live.de>
2024-10-30 13:22:45 +0100

    change of object fields to match api backend

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -2,12 +2,14 @@ import {FaXmark} from 'react-icons/fa6';
 import {useState} from 'react';
 
 const EventForm = () => {
-    const [{title, location, date, image, description}, setFormState] = useState({
+    const [{title, description, imgurl, date, location, latitude, longitude}, setFormState] = useState({
         title: '',
-        location: '',
-        date: '',
-        image: '',
         description: '',
+        imgurl: '',
+        date: '',
+        location: '',
+        latitude: 8.404746955649602,
+        longitude: 49.01438194665317
     });
 
     const handleChange = e => {
@@ -45,8 +47,8 @@ const EventForm = () => {
                         <label htmlFor="date"></label>
                     </div>
                     <div className="input-field">
-                        <input name="image" type="text" autoComplete="off" placeholder="https://via.assets.so/img.jpg?w=500&h=300" value={image} onChange={handleChange} />
-                        <label htmlFor="image">Image</label>
+                        <input name="imgurl" type="text" autoComplete="off" placeholder="https://via.assets.so/img.jpg?w=500&h=300" value={imgurl} onChange={handleChange} />
+                        <label htmlFor="imgurl">Image</label>
                     </div>
                     <div className="input-field">
                         <textarea name="description" placeholder="Outstanding description" value={description} onChange={handleChange} required />
